47f9b745757355cac6aa6f9142b439fa4feb8701
Moisés Escurra <moisesmagno@gmail.com>
2021-02-17 04:51:05 -0300

    Efetuado melhorias no retornos dos dados dos produtos.

---
+++ database/migrations/2021_02_17_031355_create_orders_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('customer_name');
+            $table->unsignedBigInteger('product_id');
+            $table->integer('order_number');
+            $table->text('observation');
+            $table->char('status', 1);
+            $table->nullableTimestamps();
+
+            $table->foreign('product_id')->references('id')->on('products');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
